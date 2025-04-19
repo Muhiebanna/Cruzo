9115963316a2be8f72ae747049d7eaa7ce73fede
Muhiebanna <muhie@DESKTOP-CEAF41B.localdomain>
2025-04-19 06:22:42 +0200

    Fix: make gradlew executable for Codemagic

--- simplified_android_project/gradlew.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-rem ------------------------------------------------------------------------------
-rem Gradle startup script for Windows
-rem ------------------------------------------------------------------------------
-
-set DIR=%~dp0
-set CLASSPATH=%DIR%gradle\wrapper\gradle-wrapper.jar
-
-java -classpath "%CLASSPATH%" org.gradle.wrapper.GradleWrapperMain %*
